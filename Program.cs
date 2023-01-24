ca3a3663787aeba1377cc5024cc861b0423eb8e9
Alex <best1972@list.ru>
2023-01-24 19:05:48 +0300

    made task 2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ Console.Write("Введите количество столбцов: ");
 int columns = int.Parse(Console.ReadLine()!);
 double[,] array = GetArray(rows, columns, -10, 10); 
 PrintArray(array);
-// -------------------Методы--------------------------------- 
+// Methods---------------------------------------------------- 
 double[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     double[,] result = new double[m, n];
@@ -32,5 +32,65 @@ void PrintArray(double[,] array)
         }
         Console.WriteLine();
     }
+}*/
+//Задача 50. Напишите программу, которая на вход принимает число, и проверяет есть ли такое число 
+//в двумерном массиве, а также возвращает сообщение о том, что оно найдено или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+/*Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Задайте число от 0 до 20: ");
+int seekNumber = int.Parse(Console.ReadLine()!);
+ Console.WriteLine();
+int[,] array = GetArray(rows, columns, 0, 20); 
+PrintArray(array);
+Console.WriteLine();
+if(seek (seekNumber) == true)
+{
+  Console.WriteLine("Заданное число найдено");  
+}
+else 
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
+//Methods---------------------------------------------------- 
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+bool seek(int seekNumber)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == seekNumber) return true;
+        }
+    }
+    return false;
 }*/
 
